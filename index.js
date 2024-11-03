575388a55129580ec26972220cebd2a1297d91a7
OmarE14 <omar.elanany@ontariotechu.net>
2024-11-03 17:51:54 -0500

    downloaded connect

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Wrld!\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log('Server running at http://${hostname}:${port}/');
-});
+// require module
+var connect = require('connect')
+ 
+// create app
+var app = connect()
 
 
-//hello lll
+function hellowrld(req,res,next){
+
+res.setHeader('Content-Type','text/plain',);
+res.end('hello wrld!');
+
+
+
+app.use(hellowrld);
+app.listen(3000);
+
+
+
+console.log("severs running on local host. ");
+}
