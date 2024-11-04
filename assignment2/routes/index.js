77fd23f6ececcd3d17d397b5ebe4479b1f9c6156
OmarE14 <omar.elanany@ontariotechu.net>
2024-11-04 08:57:22 -0500

    added bootstrap and footawesome

--- assignment2/routes/index.js
+++ assignment2/routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
 });
 
 module.exports = router;
